a6bd41e6d4c5a9d8b62c6fc190a7ab5c9dd50c42
Maribel Duran <maribelduranucm@gmail.com>
2017-05-23 22:10:04 -0700

    Added StartGame and resetGame methods to the controller object.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const MAX_PLAYERS = 2;
+
 function TicTacToe(){
 	this.player1 = {};
 	this.player2 = {};
@@ -16,12 +18,21 @@ function player(name, symbol){
 	this.score = 0;
 }
 
-TicTactoToe.prototype.setPlayer = function(player, symbol){
-	this.player1 = new player(player, symbol);
-	this.player2 = new player("computer", (player1.symbol == "X") ? "O": "X");
+TicTacToe.prototype.setPlayers = function(name, symbol){
+	this.player1 = new player(name, symbol.toUpperCase());
+	this.player2 = new player("computer", (this.player1.symbol.toUpperCase() == "X") ? "O": "X");
+
+	//When a new game is started, the first turn is picked randomly.
+	var getTurn = flipCoin(0,MAX_PLAYERS);
+	this.currentPlayer = (getTurn == 0) ? this.player1 : this.player2;
 };
 
-TicTactoToe.prototype.play = function(player, row, col){
+//returns a random number between 0 and max-1
+function flipCoin(min, max){
+ 	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+TicTacToe.prototype.play = function(player, row, col){
 //update board with the location the player chose
 	if (player == "computer"){
 		var position = getMove();
@@ -45,12 +56,12 @@ TicTactoToe.prototype.play = function(player, row, col){
 };
 
 //loop through all of the rows to see if there is a  horizontal, vertical, or diagonal line of 3 of one's own discs    
-TicTactoToe.prototype.checkWinner = function(){
+TicTacToe.prototype.checkWinner = function(){
 	return false;
 }
 
 //returns an array that represents the row and column position
-TicTactoToe.prototype.getMove = function(){
+TicTacToe.prototype.getMove = function(){
 	var position = {
 		"row": 0,
 		"col": 0
@@ -58,12 +69,13 @@ TicTactoToe.prototype.getMove = function(){
 	return position;
 }
 
-TicTactoToe.prototype.reset = function(){
+TicTacToe.prototype.reset = function(){
 	//clear all properties
 	this.player1 = {};
 	this.player2 = {};
 	this.currentPlayer = {};
 	this.finished = false;
+	this.winner = {};
 	this.board = [
 	[0,0,0],
 	[0,0,0],
@@ -77,20 +89,48 @@ var model = {
 	initializeGame: function(){
 		this.game = new TicTacToe();
 		console.log("Initialized a new TicTacToe game! ")
+		return this.game;
+	}
+	resetGame: function(){
+		this.game.reset();
 	}
 };
 
 var controller = {
 	initializeGame: function(){
-		model.initializeGame();
+		console.log(model.initializeGame());
 	},
-	setPlayer: function(symbol){
-		model.game.setPlayer(symbol);
+	setPlayers: function(name, symbol){
+		model.game.setPlayers(name, symbol);
+		console.log("Player1 is: ", model.game.player1.name, model.game.player1.symbol);
+		console.log("Player2 is: ", model.game.player2.name, model.game.player2.symbol);
+		console.log("Current player is: ", model.game.currentPlayer.name, model.game.currentPlayer.symbol);
+	},
+	startGame: function(){
+		//Need a way to use setTimeout and clearTimeout to implement this function
+		//while the game is not finished continue switching between players and let them select a move
+		while(!model.game.finished){
+			//if the curent player is the computer, then disable the board and let currentPlayer take a turn.
+			if (model.game.currentPlayer.name == "computer"){
+				model.game.play("computer");
+			}
+			else{
+				//else enable the board game and let current player take a turn
+			//once the current player selects a move then pass it to the move to model.game.play();
+			}
+		}
+		//Once the game is finished, display who won or if it was a tie. 
+		//Change current player to winner, and start game again
+		//if game is reset, then stop game and display option to let player1 choose their symbol
+	},
+	resetGame: function(){
+		//clearTimeout
+		model.game.resetGame();
 	}
 };
 
 //1) initialize game when this script loaded
-//controller.initializeGame();
+controller.initializeGame();
 //2) Player 1 chooses whether they want to play as "X" or "O";
 //3) Start game after player 1 chooses their symbol
 //4) Check if player1 goes first or player 2 goes first then 
